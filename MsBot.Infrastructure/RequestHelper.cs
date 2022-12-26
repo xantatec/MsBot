45b43947229369fcd43c214e2cb0998af6c19e59
caigy <cgy@d9tec.com>
2022-12-26 18:01:03 +0800

    Template

--- MsBot.Infrastructure/RequestHelper.cs
+++ MsBot.Infrastructure/RequestHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using HtmlAgilityPack;
+using System.Net;
 using System.Text;
 using System.Web;
 
@@ -35,7 +36,7 @@ public class RequestHelper
         string contentType = "application/json", object parameter = null,
         IDictionary<string, object> headers = null, params Cookie[] cookies)
     {
-        if (string.IsNullOrEmpty(url))
+        if(string.IsNullOrEmpty(url))
         {
             return null;
         }
@@ -45,16 +46,16 @@ public class RequestHelper
         string rspText = null;
         try
         {
-            if ((method.ToUpper() == "GET" || method.ToUpper() == "DELETE") && parameter != null)
+            if((method.ToUpper() == "GET" || method.ToUpper() == "DELETE") && parameter != null)
             {
-                if (parameter is Dictionary<string, string> urlDic)
+                if(parameter is Dictionary<string, string> urlDic)
                 {
                     url = GetRequestUrl(url, urlDic);
                 }
                 else
                 {
                     urlDic = ObjectHelper.Instance.GetDynamicObjectValues(parameter);
-                    if (urlDic != null)
+                    if(urlDic != null)
                     {
                         url = GetRequestUrl(url, urlDic);
                     }
@@ -64,12 +65,12 @@ public class RequestHelper
             req = (HttpWebRequest)System.Net.WebRequest.Create(url);
             req.Method = method.ToUpper();
             req.AllowAutoRedirect = false;
-            if (headers != null && headers.Count > 0)
+            if(headers != null && headers.Count > 0)
             {
-                foreach (var item in headers)
+                foreach(var item in headers)
                 {
                     var k = item.Key;
-                    switch (k.ToLower())
+                    switch(k.ToLower())
                     {
                         case "user-agent":
                             req.UserAgent = item.Value.ToString();
@@ -87,27 +88,27 @@ public class RequestHelper
                 }
             }
 
-            if (cookies != null && cookies.Length > 0)
+            if(cookies != null && cookies.Length > 0)
             {
-                foreach (var ck in cookies)
+                foreach(var ck in cookies)
                 {
                     req.CookieContainer.Add(ck);
                 }
             }
 
-            if (timeOut != null && timeOut.Value > 0)
+            if(timeOut != null && timeOut.Value > 0)
             {
                 req.Timeout = timeOut.Value;
             }
 
-            if (req.Method == "POST" && parameter != null)
+            if(req.Method == "POST" && parameter != null)
             {
-                if (contentType != null)
+                if(contentType != null)
                 {
                     req.ContentType = contentType;
-                    if (contentType.ToLower() == "application/json")
+                    if(contentType.ToLower() == "application/json")
                     {
-                        if (parameter is string postStringData)
+                        if(parameter is string postStringData)
                         {
                             var postBytes = Encoding.UTF8.GetBytes(postStringData);
                             req.ContentLength = postBytes.Length;
@@ -123,16 +124,16 @@ public class RequestHelper
                             rs.Write(postBytes, 0, postBytes.Length);
                         }
                     }
-                    else if (contentType == "application/x-www-form-urlencoded")
+                    else if(contentType == "application/x-www-form-urlencoded")
                     {
-                        if (parameter is Dictionary<string, string> postDic)
+                        if(parameter is Dictionary<string, string> postDic)
                         {
                             SetRequestSteam(req, postDic);
                         }
                         else
                         {
                             postDic = ObjectHelper.Instance.GetDynamicObjectValues(parameter);
-                            if (postDic != null)
+                            if(postDic != null)
                             {
                                 SetRequestSteam(req, postDic);
                             }
@@ -145,7 +146,7 @@ public class RequestHelper
             var responseStream = rsp.GetResponseStream();
             var streamReader =
                 new StreamReader(responseStream ?? throw new InvalidOperationException(), Encoding.UTF8);
-            if (rsp.StatusCode == HttpStatusCode.OK)
+            if(rsp.StatusCode == HttpStatusCode.OK)
             {
                 rspText = streamReader.ReadToEnd();
             }
@@ -190,7 +191,7 @@ public class RequestHelper
     /// <param name="fileName">文件名</param>
     public void DownloadFile(string url, string savePath, string fileName)
     {
-        if (string.IsNullOrEmpty(url))
+        if(string.IsNullOrEmpty(url))
             return;
 
         HttpWebRequest req = null;
@@ -202,15 +203,15 @@ public class RequestHelper
             req.AllowAutoRedirect = false;
             rsp = (HttpWebResponse)req.GetResponse();
             var responseStream = rsp.GetResponseStream();
-            if (responseStream == null)
+            if(responseStream == null)
                 throw new InvalidOperationException();
 
-            if (!Directory.Exists(savePath))
+            if(!Directory.Exists(savePath))
                 Directory.CreateDirectory(savePath);
 
             var newFileName = Path.Combine(savePath, fileName);
 
-            using (var fs = new FileStream(newFileName, FileMode.Create, FileAccess.Write))
+            using(var fs = new FileStream(newFileName, FileMode.Create, FileAccess.Write))
             {
                 responseStream.CopyTo(fs);
             }
@@ -219,14 +220,57 @@ public class RequestHelper
             req.Abort();
             rsp.Close();
         }
-        catch (Exception ex)
+        catch(Exception ex)
         {
-            if (req != null)
+            if(req != null)
                 req.Abort();
-            if (rsp != null)
+            if(rsp != null)
                 rsp.Close();
         }
 
         return;
+    }
+
+    public HtmlDocument HtmlAgilityPack(string url)
+    {
+        if(string.IsNullOrEmpty(url))
+            return;
+
+        HttpWebRequest req = null;
+        HttpWebResponse rsp = null;
+        try
+        {
+            req = (HttpWebRequest)System.Net.WebRequest.Create(url);
+            req.Method = "GET";
+            req.AllowAutoRedirect = false;
+            rsp = (HttpWebResponse)req.GetResponse();
+            var responseStream = rsp.GetResponseStream();
+            if(responseStream == null)
+                throw new InvalidOperationException();
+
+            using(var streamReader = new StreamReader(responseStream ?? throw new InvalidOperationException(), Encoding.UTF8))
+            {
+                if(rsp.StatusCode == HttpStatusCode.OK)
+                {
+                    var rspText = streamReader.ReadToEnd();
+                    var doc = new HtmlDocument();
+                    doc.LoadHtml(rspText);
+                    return doc;
+                }
+            }
+            
+            responseStream.Close();
+            req.Abort();
+            rsp.Close();
+        }
+        catch(Exception ex)
+        {
+            if(req != null)
+                req.Abort();
+            if(rsp != null)
+                rsp.Close();
+        }
+
+        return null;
     }
 }
