6cdb055cc74b0170a5c1bfe447003c3fcbb42567
caigy <cgy@d9tec.com>
2022-12-29 18:36:43 +0800

    Local Memory Cache

---
+++ MsBot.Infrastructure/CacheHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+
+namespace MsBot.Infrastructure
+{
+    public class CacheHelper
+    {
+        private static CacheHelper _instance;
+
+        private CacheHelper()
+        {
+            var cacheOptions = Options.Create(new MemoryCacheOptions());
+            LookupCache = new MemoryCache(cacheOptions);
+        }
+
+        private IMemoryCache LookupCache { get; set; }
+
+        /// <summary>
+        /// 实例
+        /// </summary>
+        public static CacheHelper Instance => _instance ??= new CacheHelper();
+
+        public void Add<T>(T obj,)
+        { 
+        }
+    }
+}
