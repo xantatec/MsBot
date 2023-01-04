d4bb181269905d3636910430d16d85fdb0b13dc3
caigy <cgy@d9tec.com>
2023-01-04 18:21:15 +0800

    ef

---
+++ MsBot.Implementation/MySql/Repositories/MsgSummaryRepository.cs
@@ -0,0 +1,50 @@
+﻿using MsBot.Domain;
+using MsBot.Domain.Msg;
+
+namespace MsBot.Implementation.MySql.Repositories;
+
+public class MsgSummaryRepository : Repository<MsgSummaryAggregateRoot>
+{
+    public MsgSummaryRepository(IRepositoryContextProvider contextProvider)
+        : base(contextProvider)
+    {
+    }
+
+    public Dictionary<int, long> GetTodayCount(int year, int month, int day)
+    {
+        var query = MsBotContext.MsgSummaries.Where(s => s.Year == year && s.Month == month && s.Day == day).ToList();
+        var result = new Dictionary<int, long>();
+        foreach(var kv in query)
+            result.Add(kv.Hour, kv.Count);
+
+        return result;
+    }
+
+    public Dictionary<int, long> GetMonthCount(int year, int month)
+    {
+        var query = MsBotContext.MsgSummaries.Where(s => s.Year == year && s.Month == month).GroupBy(s => s.Day).Select(s => new
+        {
+            Day = s.Key,
+            Count = s.Sum(ss => ss.Count)
+        }).ToList();
+        var result = new Dictionary<int, long>();
+        foreach(var kv in query)
+            result.Add(kv.Day, kv.Count);
+
+        return result;
+    }
+
+    public Dictionary<int, long> GetYearCount(int year)
+    {
+        var query = MsBotContext.MsgSummaries.Where(s => s.Year == year).GroupBy(s => s.Month).Select(s => new
+        {
+            Month = s.Key,
+            Count = s.Sum(ss => ss.Count)
+        }).ToList();
+        var result = new Dictionary<int, long>();
+        foreach(var kv in query)
+            result.Add(kv.Month, kv.Count);
+
+        return result;
+    }
+}
